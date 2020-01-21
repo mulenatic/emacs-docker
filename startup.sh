bbb71fc009e074226b56a43425461e170e4a01bb
user <user>
2020-01-21 13:54:27 +0000

    Changed the startup script, if no volume is given, the git repo which is given as first parameter is checked out and opened.

--- startup.sh
+++ startup.sh
@@ -5,23 +5,26 @@ EMACS_DIR="/emacsDocuments";
 if [ ! -d "$EMACS_DIR" ];
 then
     echo "No volume bound to $EMACS_DIR. Please start the container with \"-v <desired path to directory on the host>:$EMACS_DIR\"";
-    exit 1
+
+    if [ $# -eq 1 ];
+    then
+	EMACS_DIR=$HOME
+	echo "Checking out repository: "
+	cd ~;
+	git clone $1;
+    else     
+	exit 1
+    fi
 fi
 
 #TestAddons
 
-echo "List of given parameters, will be used as files which will be opened by emacs from directory $EMACS_DIR: $@"
-
 declare -a files=("$EMACS_DIR/");
 
-for file in $@
-do
-    files=("$files" "$EMACS_DIR/$file");
-done
-
-echo $files[0] $files[1]
-echo "${files[@]}"
-echo "${files[@]/#/ }"
+#for file in $@
+#do
+#    files=("$files" "$EMACS_DIR/$file");
+#done
 
 #scriptAddons
 
